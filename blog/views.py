0dfacdb7c4fada4a2ed2d01219e0cf287d9f9231
Nlansong <brightwumbei2018@gmail.com>
2023-09-01 17:46:36 +0000

    added deploy commands

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
+from news.models import PostModel
 
 
 # Create your views here.
 def home(request):
-    return render(request, 'blog/home.html')
+    posts = PostModel.objects.all()[0:3]
+    context = {
+        'title':'McGill University',
+        'posts':posts
+    }
+    return render(request, 'blog/home.html', context)
--- news/urls.py
+++ news/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from .views import news_index
 
+app_name = 'news'
+
 urlpatterns = [
     path('',news_index,name='index'),
 ]
