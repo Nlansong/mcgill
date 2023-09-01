abd144de9f98e41551fa089dba7696ae062a0163
Nlansong <brightwumbei2018@gmail.com>
2023-09-01 18:52:10 +0000

    added

--- homepage/settings.py
+++ homepage/settings.py
@@ -14,9 +14,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-hz!$-(5mysx9yafovs74lx6grsr0i$c23&uo)dc#enlbx*uzb)'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
